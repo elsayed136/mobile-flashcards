c635a9377ac02072ca057e66ab3c5e10a91d9302
elsayed136 <elsayed.ahmed5757@outlook.com>
2021-03-05 15:38:12 +0200

    style: Adding some colors

--- utils/colors.js
+++ utils/colors.js
@@ -2,10 +2,12 @@
 
 export const purple = '#292477'
 export const gray = '#757575'
+export const lightGray = 'rgba(0,0,0,0.1)'
 export const white = '#fff'
 export const black = '#000'
-export const red = '#b71845'
+export const red = '#D4271B'
 export const orange = '#f26f28'
+export const green = '#008000'
 export const blue = '#4e4cb8'
 export const lightPurp = '#7c53c3'
 export const pink = '#b93fb3'
