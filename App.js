075041d1a906e8c6e8c899d2e3baa05d0ceab222
elsayed136 <elsayed.ahmed5757@outlook.com>
2021-03-07 09:51:18 +0200

    feat: Add store

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import { View } from 'react-native'
+
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import thunk from 'redux-thunk'
+
 import CustomStatusBar from './components/common/CustomStatusBar'
 import Navigator from './navigation'
 import { lightGray } from './utils/colors'
 
+const store = createStore(reducer, applyMiddleware(thunk))
+
 const App = () => {
   return (
-    <View style={{ flex: 1 }}>
-      <CustomStatusBar backgroundColor={lightGray} barStyle='light-content' />
-      <Navigator />
-    </View>
+    <Provider store={store}>
+      <View style={{ flex: 1 }}>
+        <CustomStatusBar backgroundColor={lightGray} barStyle='light-content' />
+        <Navigator />
+      </View>
+    </Provider>
   )
 }
 
